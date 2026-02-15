8b4a224c2ad226520be14188df6ca6f77ee9f334
Florian Royon-Chalendard <florian.royon@gmail.com>
2026-02-15 20:59:20 +0100

    Modification Géométrie et Maillage GMSH pour Tuyère
    Interpolation spline cubique pour la géométrie et maillage GMSH avec Raffinement proche paroi pour garder ~50k Nœuds

--- src/lavalNozzle/generate_nozzle.py
+++ src/lavalNozzle/generate_nozzle.py
@@ -1,11 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+from scipy.interpolate import PchipInterpolator
 
 
 def generate_nozzle(R_throat, R_exit,R_inlet, L_divergent, L_convergent):
     """
-    Génère une tuyère de Laval avec les paramètres suivants :
+    Génère une demi-tuyère de Laval avec les paramètres suivants :
     - R_inlet : Rayon de l'entrée
     - R_throat : Rayon du col
     - R_exit : Rayon de sortie
@@ -13,23 +13,15 @@ def generate_nozzle(R_throat, R_exit,R_inlet, L_divergent, L_convergent):
     - L_convergent : Longueur du convergent
     """
     
-    x_convergent = np.linspace(0, L_convergent, 100)
-    x_divergent = np.linspace(L_convergent, L_convergent + L_divergent, 100)
+    # Points de contrôle de la courbe
+    x_control = np.array([0, L_convergent, L_convergent + L_divergent])
+    y_control = np.array([R_inlet, R_throat, R_exit])
 
-    coef_convergent = (R_throat - R_inlet) / L_convergent
-    coef_divergent = (R_exit - R_throat) / L_divergent
+    # Interpolation avec spline cubique
+    spline = PchipInterpolator(x_control, y_control)
 
-    y_convergent = R_inlet + coef_convergent * x_convergent
-    y_divergent = R_throat + coef_divergent * (x_divergent - L_convergent)
-
-    x_pos = np.concatenate([x_convergent, x_divergent])
-    y_pos = np.concatenate([y_convergent, y_divergent])
-
-    y_neg = -y_pos
-    x_neg = x_pos
-
-    x = np.concatenate([x_pos, x_neg])
-    y = np.concatenate([y_pos, y_neg])
+    x = np.linspace(0, L_convergent + L_divergent, 100)
+    y = spline(x)
     
     return x, y
 
---
+++ src/lavalNozzle/mesh_gen.py
@@ -0,0 +1,115 @@
+import os
+import subprocess
+
+def create_geo_file(nozzle_file, output_geo,l_convergent, lc_tuyere=0.0015, lc_col=0.001, lc_domain=0.5, l_domain=2.0, r_exit=4.0):
+    """
+    Crée un fichier Gmsh .geo avec raffinement local au bord d'attaque et bord de fuite.
+    """
+    points = []
+    with open(nozzle_file, 'r') as f:
+        for line in f:
+            points.append(list(map(float, line.split())))
+    
+    with open(output_geo, 'w') as f:
+        f.write(f"lc_tuyere = {lc_tuyere};\n")
+        f.write(f"lc_domain = {lc_domain};\n\n")
+        f.write(f"l_domain = {l_domain};\n")
+        f.write(f"r_exit = {r_exit};\n\n")
+        l_outlet = l_domain + l_domain
+        f.write(f"l_outlet = {l_outlet};\n")
+        
+        # Points de la tuyère 
+        for i, (x, y) in enumerate(points):
+            f.write(f"Point({i+1}) = {{{x}, {y}, 0, lc_tuyere}};\n")
+        
+        # Spline de la tuyère Convergente
+        point_ids = ", ".join([str(i+1) for i in range(len(points))])
+        f.write(f"Spline(1) = {{{point_ids}}};\n\n")
+        
+        # Points du domaine (Champ lointain et ligne de symétrie)
+
+        f.write(f"Point(1001) = {{0.0, 0.0, 0.0, lc_domain}};\n")
+        f.write(f"Point(1002) = {{l_domain, 0.0, 0.0, lc_domain}};\n")
+        f.write(f"Point(1003) = {{l_outlet, 0.0, 0.0, lc_domain}};\n")    
+        f.write(f"Point(1004) = {{l_outlet, r_exit, 0.0, lc_domain}};\n\n")
+
+        
+        # Lignes du domaine
+        f.write("Line(2) = {1, 1001};\n")
+        f.write("Line(3) = {1001, 1002};\n")
+        f.write("Line(4) = {1002, 1003};\n")
+        f.write("Line(5) = {1003, 1004};\n")
+        f.write(f"Line(6) = {{1004, {len(points)}}};\n\n")
+        
+
+        # Surface Domaine
+        f.write("Curve Loop(1) = {3, 4, 5, 6, -1, 2};\n")
+        f.write("Plane Surface(1) = {1};\n\n")
+
+        # 2. Identifier l'index du col (le point le plus proche de l_convergent)
+        throat_idx = 1 # Par défaut point 1
+        min_diff = float('inf')
+        for i, pt in enumerate(points):
+            diff = abs(pt[0] - l_convergent)
+            if diff < min_diff:
+                min_diff = diff
+                throat_idx = i + 1 
+
+        f.write(f"throat_idx = {throat_idx};\n")
+
+        # --- STRATÉGIE DE RAFFINEMENT (Dist + Threshold) ---
+        # 1. On calcule la distance à la tuyère
+        f.write("Field[1] = Distance;\n")
+        f.write("Field[1].CurvesList = {1};\n")
+        f.write("Field[1].Sampling = 100;\n")
+
+        # 2. On définit une zone de maillage fin (lc_tuyere) proche de la paroi
+        f.write("Field[2] = Threshold;\n")
+        f.write("Field[2].IField = 1;\n")
+        f.write(f"Field[2].LcMin = {lc_tuyere};\n")      # Taille au contact
+        f.write(f"Field[2].LcMax = {lc_domain};\n")      # Taille loin
+        f.write("Field[2].DistMin = 0.02;\n")            # Fin jusqu'à 5cm
+        f.write("Field[2].DistMax = 0.2;\n")             # Transition vers le gros maillage
+
+        # 3. On raffine spécifiquement le COL (Throat) pour capturer Mach 1
+        # On utilise le point du col (milieu du tableau points)
+        f.write("Field[3] = Distance;\n")
+        f.write(f"Field[3].NodesList = {{throat_idx}};\n") 
+        f.write("Field[4] = Threshold;\n")
+        f.write("Field[4].IField = 3;\n")
+        f.write(f"Field[4].LcMin = {lc_col};\n") # Deux fois plus fin au col
+        f.write(f"Field[4].LcMax = {lc_domain};\n")
+        f.write("Field[4].DistMin = 0.1;\n")
+        f.write("Field[4].DistMax = 0.3;\n")
+
+        # 4. On prend le minimum de tous les raffinements
+        f.write("Field[5] = Min;\n")
+        f.write("Field[5].FieldsList = {2, 4};\n")
+        f.write("Background Field = 5;\n\n")
+
+        f.write("Mesh.Algorithm = 6;\n") # Frontal-Delaunay (très bon pour GNN)
+
+        # Extruder pour OpenFOAM
+        f.write("out[] = Extrude {0, 0, 0.1} {\n")
+        f.write("  Surface{1,2}; Layers{1}; Recombine;\n")
+        f.write("};\n\n")
+        
+        # Groupes physiques
+        f.write("Physical Surface(\"front\") = {1};\n")
+        f.write("Physical Surface(\"back\") = {out[0]};\n")
+        f.write("Physical Surface(\"inlet\") = {out[7]};\n")
+        f.write("Physical Surface(\"outlet\") = {out[4],out[5]};\n")
+        f.write("Physical Surface(\"nozzle\") = {out[6]};\n")
+        f.write("Physical Surface(\"symmetry\") = {out[2],out[3]};\n")
+        f.write("Physical Volume(\"internal\") = {out[1]};\n")
+        
+def run_gmsh(geo_file, msh_file):
+    cmd = ["gmsh", "-3", geo_file, "-o", msh_file, "-format", "msh2"]
+    subprocess.run(cmd, check=True)
+
+if __name__ == "__main__":
+    nozzle_dat = "data/nozzle/nozzle_14211.dat"
+    geo_file = "data/mesh.geo"
+    msh_file = "data/mesh.msh"
+    create_geo_file(nozzle_dat, geo_file,l_convergent=1, lc_tuyere=0.005, lc_col=0.002, lc_domain=0.1, l_domain=2.0, r_exit=4.0)
+    run_gmsh(geo_file, msh_file)
